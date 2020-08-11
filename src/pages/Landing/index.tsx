a16a25c845e486777b2a58078945800f4864760c
Joao-Meyer <joaoparizotimeyer@gmail.com>
2020-08-11 19:56:59 -0300

    Mudança na logo (4).

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -7,7 +7,7 @@ import styles from './styles';
 function Landing () {
     return (
         <View style={styles.container}>
-            <Text style={styles.titulo}>AirBnBand 3</Text>
+            <Text style={styles.titulo}>AirBnBand 4</Text>
         </View>
     )
 }
