5487e4ef20d97a0b28f961911efb008a64e85774
vithor97 <vitorsantos27@hotmail.com>
2020-08-11 13:36:42 -0300

    Mudança no backgroumdColor

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -7,7 +7,7 @@ import styles from './styles';
 function Landing () {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>AirBnBand</Text>
+            <Text style={styles.titulo}>AirBnBand</Text>
         </View>
     )
 }
--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -5,7 +5,7 @@ import {StyleSheet } from 'react-native'
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#F25F16',
+        backgroundColor: '#FD9A0B',
         alignItems: 'center',
         justifyContent: 'center',
       },
@@ -13,6 +13,15 @@ const styles = StyleSheet.create({
           fontFamily: 'sans-serif-medium',
           fontSize: 20,
           color: '#F1F1F1'
+      },
+      titulo: {
+
+        alignContent: 'center',
+        height: 250,
+        fontSize: 48,
+        fontFamily: 'sans-serif-medium',
+        color: '#FFFFFF'
+
       }
 });
 
