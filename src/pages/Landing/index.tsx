8c97a3999e80deb5ad3186f55c821f22b6a4548f
vithor97 <vitorsantos27@hotmail.com>
2020-08-11 13:12:24 -0300

    First Commit

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+
+import styles from './styles';
+
+function Landing () {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>AirBnBand</Text>
+        </View>
+    )
+}
+
+export default Landing;
---
+++ src/pages/Landing/styles.ts
@@ -0,0 +1,20 @@
+import {StyleSheet } from 'react-native'
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F25F16',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      title: {
+          fontFamily: 'sans-serif-medium',
+          fontSize: 20,
+          color: '#F1F1F1'
+      }
+});
+
+
+export default styles;
