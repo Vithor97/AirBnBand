8b19f2408c677c74f4c6c990ddfcacfbc1b88d4a
vithor97 <vitorsantos27@hotmail.com>
2020-08-11 19:59:14 -0300

    teste 2(vitor)

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -7,7 +7,7 @@ import styles from './styles';
 function Landing () {
     return (
         <View style={styles.container}>
-            <Text style={styles.titulo}>AirBnBand 4</Text>
+            <Text style={styles.titulo}>AirBnBand 5</Text>
         </View>
     )
 }
