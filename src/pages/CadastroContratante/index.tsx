e84bad704bc70e6e37b587f545f96d2616c57f43
vithor97 <vitorsantos27@hotmail.com>
2020-09-21 19:23:35 -0300

    Form de validacao no cadastro contratante

--- src/pages/CadastroContratante/index.tsx
+++ src/pages/CadastroContratante/index.tsx
@@ -1,17 +1,73 @@
-import React, { useState } from 'react';
-import { View, Image } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, Image, ScrollView, Text} from 'react-native';
+import ViewPager  from '@react-native-community/viewpager';
+import {Formik} from 'formik';
+import { TextInput } from 'react-native-gesture-handler';
+import { ProgressBallsContainer, ProgressBallFilled, ProgressBallEmpty } from '../../components/progressBalls';
+import * as Yup from "yup";
 
 import BackArrow from '../../components/backArrow';
 import DivisorBar from '../../components/divisorBar';
 
-import Etapa1 from './etapa_1';
-import Etapa2 from './etapa_2';
-import Etapa3 from './etapa_3';
+import strings from '../../resources/values/strings.json';
+import colors from '../../resources/values/colors.json';
+import ConfirmaBtn from '../../components/confirmaBtn';
+import NextArrowButton from '../../components/nextArrowButton';
+import MensagemErro from '../../components/errorMessage'
+import PhotoInput from '../../components/photoInput';
 
 import styles from './styles';
 import global from '../../styles/global';
 
 function CadastroContratante () {
+
+    let [page, setPagee] = useState(0)
+
+    const viewPager  = useRef<ViewPager | null | HTMLInputElement | any>();
+    //Validacao de formulario
+    const FormSchema = Yup.object().shape({
+        nome: Yup.string().min(4, "Minimun length of 4").required("Required"),
+        email: Yup.string().email("Invalid email").required("Required"),
+        senha: Yup.string().min(4, "Minimun length of 8").required("Required"),
+        senhaRepete: Yup.string()
+        .oneOf([Yup.ref("senha")], "Senhas devem ser iguais")
+        .min(4, "Minimun length of 4")
+        .required("Required"),
+        cep:Yup.number().required("Obrigatório").typeError("Somente numeros"),
+        logradoro: Yup.string().required("Obrigatório"),
+        cidade: Yup.string().required("Obrigatório"),
+        bairro: Yup.string().required("Obrigatório") ,
+        numero: Yup.string().required("Obrigatório"),
+        cnpj: Yup.string().required("Obrigatório"),
+        telefone: Yup.string().required("Obrigatório") ,
+        nomeArtistico: Yup.string().required("Obrigatório"),
+    })
+
+    
+    function btnAvancaViewPager(){
+        if(page>=2) {
+            // viewPager.current.setPage(0)
+            // viewPager.current.setPage(page)
+            // Implementar a conclusão do cadastro
+            //goToHome();
+        }
+        else{
+            viewPager.current.setPage(page+1)  
+        }          
+    }
+
+    function estadoScroll (e:any) {
+        let valorPage = e.nativeEvent.position;
+        setPagee(valorPage);
+
+        console.log(`numero da pagina: ${valorPage}`);
+    }
+
+
+    function mensagemDeErro(e: string){
+        return <MensagemErro errorMensgame ={e}/>
+    }
+
     return (
         <View style={styles.container}>
             <View style={global.headerContainer}>
@@ -28,7 +84,213 @@ function CadastroContratante () {
                 <DivisorBar />
             </View>
 
-            <Etapa3 />
+            <View style={styles.contentContainer}>
+                <Formik 
+                initialValues={{
+                    nome: "",
+                    email: "",
+                    senha: "",
+                    senhaRepete: "",
+                    cnpj: "",
+                    telefone: "",
+                    nomeArtistico: "",
+                    cep: "",
+                    logradoro: "",
+                    cidade: "",
+                    bairro: "",
+                    numero: "",
+                }}
+                onSubmit={async(values, actions)=>{
+                        console.log(values)
+                        await console.log(actions)
+                    }
+                }
+  
+                validationSchema={FormSchema}        
+                >
+                {({values , handleChange, errors, handleSubmit, touched, setFieldTouched}) =>{
+                    //console.log({ values });
+                    return(
+                        <>
+                        <ViewPager ref={viewPager} scrollEnabled={true} onPageSelected={estadoScroll} style={styles.viewPager} initialPage={page}>
+                        <ScrollView>
+                            <View key="1">
+                                <View style={styles.inputsContainer}>
+                                    <Text style={styles.addYourData}>{strings.addYourData}</Text>
+                        
+                                    <TextInput
+                                        style={styles.textInput}
+                                        placeholder={strings.fullName}
+                                        placeholderTextColor= {colors.white}
+                                        value={values.nome}
+                                        onBlur={()=>setFieldTouched('nome', true)}
+                                        onChangeText={handleChange("nome")}
+                                    />
+                                    {errors.nome &&  touched.nome ? mensagemDeErro(errors.nome) : null}
+                        
+                                    <TextInput
+                                        style={styles.textInput}
+                                        placeholder={strings.email}
+                                        placeholderTextColor= {colors.white}
+                                        value={values.email}
+                                        onBlur={()=>setFieldTouched('email', true)}
+                                        onChangeText={handleChange("email")}
+                                    />
+                                    {errors.email &&  touched.email && mensagemDeErro(errors.email)}
+                         
+                                    <TextInput
+                                        style={styles.textInput}
+                                        placeholder={strings.password}
+                                        placeholderTextColor= {colors.white}
+                                        value={values.senha}
+                                        onBlur={()=>setFieldTouched('senha', true)}
+                                        onChangeText={handleChange("senha")}
+                                    />
+                                    {errors.senha &&  touched.senha && mensagemDeErro(errors.senha)}
+                                      
+                                    <TextInput
+                                        style={styles.textInput}
+                                        placeholder={strings.retypePassword}
+                                        placeholderTextColor= {colors.white}
+                                        value={values.senhaRepete}
+                                        onBlur={()=>setFieldTouched('senhaRepete', true)}
+                                        onChangeText={handleChange("senhaRepete")}
+                                    />
+                                    {errors.senhaRepete &&  touched.senhaRepete && mensagemDeErro(errors.senhaRepete)}
+                                </View>
+                            </View>
+                        </ScrollView>
+
+                        <ScrollView>
+                            <View key="2">        
+                                <View style={styles.inputsContainer}>
+                                    <PhotoInput />
+                        
+                                    <TextInput
+                                        style={styles.textInput}
+                                        placeholder={strings.cpfcnpj}
+                                        placeholderTextColor= {colors.white}
+                                        value={values.cnpj}
+                                        onBlur={()=>setFieldTouched('cnpj', true)}
+                                        onChangeText={handleChange("cnpj")}
+                                    />
+                                    {errors.cnpj &&  touched.cnpj && mensagemDeErro(errors.cnpj)}    
+                         
+                                    <TextInput
+                                        style={styles.textInput}
+                                        placeholder={strings.telephone}
+                                        placeholderTextColor= {colors.white}
+                                        value={values.telefone}
+                                        onBlur={()=>setFieldTouched('telefone', true)}
+                                        onChangeText={handleChange("telefone")}
+                                    />
+                                    {errors.telefone &&  touched.telefone && mensagemDeErro(errors.telefone)} 
+                         
+                                    <TextInput
+                                        style={styles.textInput}
+                                        placeholder={strings.artisticName}
+                                        placeholderTextColor= {colors.white}
+                                        value={values.nomeArtistico}
+                                        onBlur={()=>setFieldTouched('nomeArtistico', true)}
+                                        onChangeText={handleChange("nomeArtistico")}
+                                    />
+                                    {errors.nomeArtistico &&  touched.nomeArtistico && mensagemDeErro(errors.nomeArtistico)} 
+                                </View>
+                            </View>
+                        </ScrollView>
+
+                        <ScrollView>
+                            <View key="3">
+                                <View style={styles.inputsContainer}>
+                                    <TextInput
+                                        style={styles.textInput}
+                                        placeholder={strings.postalCode}
+                                        placeholderTextColor= {colors.white}
+                                        keyboardType="numeric"
+                                        value={values.cep}
+                                        onBlur={()=>setFieldTouched('cep', true)}
+                                        onChangeText={handleChange("cep")}
+                                        maxLength={10} 
+                                    />
+                                    {errors.cep &&  touched.cep && mensagemDeErro(errors.cep)}
+                
+                                    <TextInput
+                                        style={styles.textInput}
+                                        placeholder={strings.street}
+                                        placeholderTextColor= {colors.white}
+                                        value={values.logradoro}
+                                        onBlur={()=>setFieldTouched('logradoro', true)}
+                                        onChangeText={handleChange("logradoro")}
+                                    />
+                                    {errors.logradoro &&  touched.logradoro && mensagemDeErro(errors.logradoro)}
+                        
+                                    <TextInput
+                                        style={styles.textInput}
+                                        placeholder={strings.city}
+                                        placeholderTextColor= {colors.white}
+                                        value={values.cidade}
+                                        onBlur={()=>setFieldTouched('cidade', true)}
+                                        onChangeText={handleChange("cidade")}
+                                    />
+                                    {errors.cidade &&  touched.cidade && mensagemDeErro(errors.cidade)}
+                                    
+                                    <TextInput
+                                        style={styles.textInput}
+                                        placeholder={strings.block}
+                                        placeholderTextColor= {colors.white}
+                                        value={values.bairro}
+                                        onBlur={()=>setFieldTouched('bairro', true)}
+                                        onChangeText={handleChange("bairro")}
+                                    />
+                                    {errors.bairro &&  touched.bairro && mensagemDeErro(errors.bairro)}
+                                    
+                                    <TextInput
+                                        style={styles.textInput}
+                                        placeholder={strings.number}
+                                        placeholderTextColor= {colors.white}
+                                        value={values.numero}
+                                        onChangeText={handleChange("numero")}
+                                        onBlur={()=>setFieldTouched('numero', true)}
+                                        keyboardType="numeric"
+                                        maxLength={6}
+                                    />
+                                    {errors.numero &&  touched.numero && mensagemDeErro(errors.numero)}
+                                    {/* {(function() {
+                                        if(errors.numero) { 
+                                            setLiberado(false)    
+                                            return <Text>{errors.numero}</Text>;
+                                        }
+                                        else{
+                                            setLiberado(true)
+                                        } 
+                                        })()
+                                    }  */}
+                                </View>
+                            </View>
+                        </ScrollView>
+                        </ViewPager>
+                        
+                        <View style={styles.flowContainer}>
+                            <ProgressBallsContainer>
+                                {
+                                
+                                }
+                                <ProgressBallFilled />
+
+                                <ProgressBallEmpty />
+
+                                <ProgressBallEmpty />
+                            </ProgressBallsContainer>
+                            {page == 2 ? <ConfirmaBtn onNext={handleSubmit}/> : <NextArrowButton onNext={btnAvancaViewPager}/> }
+                            {/* <NextArrowButton onNext={handleSubmit}/> */}
+                        </View>
+                    </>
+                    )
+                }
+                }                    
+                </Formik>   
+
+            </View>
         </View>
     );
 }
--- src/pages/CadastroContratante/styles.ts
+++ src/pages/CadastroContratante/styles.ts
@@ -1,4 +1,5 @@
 import {StyleSheet } from 'react-native';
+import { color } from 'react-native-reanimated';
 import colors from '../../resources/values/colors.json';
 import fonts from '../../resources/values/fonts.json'
 
@@ -37,10 +38,64 @@ const styles = StyleSheet.create({
     },
 
     contentContainer: {
+        width: "100%",
+        // width: "100%",
+        height: "100%",
+        // marginTop: 20,
+        paddingTop: 10,
+
+        // backgroundColor: colors.facebookBlue,
+
+        // flex: 1,
+        // flexWrap: "wrap",
+        alignSelf: "center",
+        // justifyContent: "space-evenly",
+        display: "flex"
+    },
+
+    viewPager: {
+        display: "flex",
+        // justifyContent: "center",
+        // alignItems: "center",
+
+        flex: 1,
+        // minHeight: "45%",
+        // height: "auto",
+        // flexWrap: "wrap",
+        width: "100%",
+        // backgroundColor: colors.secundary
+    },
+
+    inputsContainer:{
         width: "85%",
-        marginTop: 20,
-        flex: 1
-    }
+        alignSelf: "center",
+        // height: "33%",
+        // flex: 1,
+        // backgroundColor: colors.black
+    },
+    
+    flowContainer: {
+        width: "100%",
+        // flex: 1,
+        height: "35%",
+        display: "flex"
+    },
+
+    addYourData: {
+        marginBottom: 10,
+        fontSize: fonts.titleFontSize,
+        color: colors.primary,
+    //   backgroundColor: colors.black
+    },
+    textInput: {
+        height: 50,
+        padding: 10,
+        paddingLeft: 20,
+        borderRadius: 25,
+        backgroundColor: colors.primary,
+        marginBottom: 10,
+        fontSize: fonts.textInputFontSize,
+      },
 });
 
 export default styles;
