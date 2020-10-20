12c8d77e2b480643753845dce5372d581943f0a9
Joao-Meyer <joaoparizotimeyer@gmail.com>
2020-10-20 17:44:03 -0300

    Estilização das delas 'DetailsProfileContratante' e 'Configuracao' e adição de recursos de cores.

--- src/pages/Configuracao/index.tsx
+++ src/pages/Configuracao/index.tsx
@@ -6,6 +6,8 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Feather from 'react-native-vector-icons/Feather';
 
+import DivisorBar from '../../components/divisorBar';
+
 import AuthContext from '../../contexts/auth';
 
 import colors from '../../resources/values/colors.json';
@@ -19,131 +21,112 @@ function Configuracao(){
     }
 
     return (
-        
         <View style={styles.container}>
             <KeyboardAvoidingView  behavior="padding">
-                <ScrollView  style={{margin:10}}>
-                    <View style={{margin: 7}}>
-                        <View style={{alignItems: 'center'}}>
-                            <TouchableOpacity onPress={()=>{}}>
-                                <View style={{
-                                    height: 100,
-                                    width: 100,
-                                    borderRadius: 15,
-                                    justifyContent: 'center',
-                                    alignItems: 'center'
-                                }}>
-
+                <ScrollView  style={styles.containerScrollView}>
+                    <View style={styles.formContainer}>
+                        <TouchableOpacity onPress={()=>{}} style={styles.profileImageContainer}>
+                            {/* <View style={styles.profileImageContainer}> */}
                                 <ImageBackground 
-                                    style={styles.avatar}
                                     imageStyle={{borderRadius: 15}}
                                     source={{uri: "https://conteudo.imguol.com.br/c/entretenimento/80/2017/04/25/a-atriz-zoe-saldana-como-neytiri-em-avatar-1493136439818_v2_1920x1200.jpg"}}
-                                >
-                                    <View style={{
-                                        flex: 1,
-                                        justifyContent: 'center',
-                                        alignItems: 'center'
-                                    }}>
+                                    style={styles.profileImage}>
+                                    <View style={styles.imageSelectIconContainer}>
                                         <Icon 
                                             name="camera" 
                                             size={35} 
-                                            color="#fff"
-                                            style={{
-                                                opacity: 0.7,
-                                                alignItems: 'center',
-                                                justifyContent: 'center',
-                                                borderWidth: 1,
-                                                borderColor: '#fff',
-                                                borderRadius: 10
-                                            }}
-                                        />
+                                            style={styles.imageSelectIcon}/>
                                     </View>
                                 </ImageBackground>
+                            {/* </View> */}
+                        </TouchableOpacity>
 
-                                </View>
-                            </TouchableOpacity>
-                            <Text style={{marginTop: 10, fontSize: 18, fontWeight: 'bold'}}>
-                                Vitor Miranda
-                            </Text>
-                
+                        <Text style={styles.profileName}>
+                            Vitor Miranda
+                        </Text>
+
+                        <View style={styles.action}>
+                            <FontAwesome name="user-o" size={20} />
+
+                            <TextInput
+                                placeholder="Nome"
+                                placeholderTextColor="#666666"
+                                style={styles.textInput}/>                
                         </View>
-                                    
+                            
+                        <View style={styles.action}>
+                            <FontAwesome name="user-o" size={20} />
 
+                            <TextInput
+                                placeholder="Sobrenome"
+                                placeholderTextColor="#666666"
+                                style={styles.textInput}/>                
+                        </View>
+
+                        <View style={styles.action}>
+                            <Feather name="phone" size={20} />
+
+                            <TextInput
+                                keyboardType='number-pad'
+                                placeholder="Telefone"
+                                placeholderTextColor="#666666"
+                                style={styles.textInput}/>                
+                        </View>
+
+                        <View style={styles.action}>
+                            <FontAwesome name="envelope-o" size={20} />
+
+                            <TextInput
+                                placeholder="Email"
+                                placeholderTextColor="#666666"
+                                style={styles.textInput}/>                
+                        </View>
+
+                        <Text style={{left: '40%', fontSize: 15, fontWeight: 'bold'}}>
+                            Localização
+                        </Text>
+
+                        <DivisorBar/>
+
+                        {/* <View style={{
+                            borderBottomWidth: 3,
+                            borderBottomColor: colors.primary,
+                            paddingBottom: 5,
+                            marginTop: 6,
+                            marginBottom: 7
+                        }}/> */}
                         
-                                <View style={styles.action}>
-                                    <FontAwesome name="user-o" size={20} />
-                                    <TextInput
-                                        placeholder="Nome"
-                                        placeholderTextColor="#666666"
-                                        style={styles.textInput}
-                                    />                
-                                </View>
-                                    
-                                <View style={styles.action}>
-                                    <FontAwesome name="user-o" size={20} />
-                                    <TextInput
-                                        placeholder="Sobrenome"
-                                        placeholderTextColor="#666666"
-                                        style={styles.textInput}
-                                    />                
-                                </View>
-                                <View style={styles.action}>
-                                    <Feather name="phone" size={20} />
-                                    <TextInput
-                                        keyboardType='number-pad'
-                                        placeholder="Telefone"
-                                        placeholderTextColor="#666666"
-                                        style={styles.textInput}
-                                    />                
-                                </View>
-                                <View style={styles.action}>
-                                    <FontAwesome name="envelope-o" size={20} />
-                                    <TextInput
-                                        placeholder="Email"
-                                        placeholderTextColor="#666666"
-                                        style={styles.textInput}
-                                    />                
-                                </View>
-                                <Text style={{left: '40%', fontSize: 15, fontWeight: 'bold'}}>localização</Text>
-                                <View style={{
-                                    borderBottomWidth: 3,
-                                    borderBottomColor: colors.primary,
-                                    paddingBottom: 5,
-                                    marginTop: 6,
-                                    marginBottom: 7
-                                }
-                                }></View>
-                                
-                                <Text>Rua: </Text>
-                                <View style={styles.action}>
-                                    <Icon name="map-marker-outline" size={20} />
-                                    <TextInput
-                                        placeholder="Endereço"
-                                        placeholderTextColor="#666666"
-                                        style={styles.textInput}
-                                    />                
-                                </View>
+                        <Text>Rua: </Text>
 
-                                <Text>Cidade: </Text>
-                                <View style={styles.action}>
-                                    <Icon name="map-marker-outline" size={20} />
-                                    <TextInput
-                                        placeholder="Cidade"
-                                        placeholderTextColor="#666666"
-                                        style={styles.textInput}
-                                    />                
-                                </View>
-                                
-                                <View style={{position: 'relative'}}>
-                                    <TouchableOpacity style={styles.commandButton}>
-                                        <Text style={styles.panelButtonTitle}>Submit</Text>
-                                    </TouchableOpacity>
-                                </View>
+                        <View style={styles.action}>
+                            <Icon name="map-marker-outline" size={20} />
+
+                            <TextInput
+                                placeholder="Endereço"
+                                placeholderTextColor="#666666"
+                                style={styles.textInput}/>                
+                        </View>
+
+                        <Text>Cidade: </Text>
+
+                        <View style={styles.action}>
+                            <Icon name="map-marker-outline" size={20} />
+
+                            <TextInput
+                                placeholder="Cidade"
+                                placeholderTextColor="#666666"
+                                style={styles.textInput}/>                
+                        </View>
+                        
+                        <View style={{position: 'relative'}}>
+                            <TouchableOpacity style={styles.commandButton}>
+                                <Text style={styles.panelButtonTitle}>Salvar</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 </ScrollView>
             </KeyboardAvoidingView>
         </View>
-
     )
 }
 
--- src/pages/DetailsProfileContratante/index.tsx
+++ src/pages/DetailsProfileContratante/index.tsx
@@ -4,6 +4,7 @@ import { View, Text, TouchableOpacity, SafeAreaView, Image} from 'react-native'
 import { useNavigation } from '@react-navigation/native';
 import AuthContext from '../../contexts/auth';
 
+import DivisorBar from '../../components/divisorBar';
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import Icon2 from 'react-native-vector-icons/FontAwesome5'
@@ -26,59 +27,65 @@ function DetailsProfileContratante(){
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.userInfoSection}>
-                <View style={{flexDirection:'row', marginTop: 15}}>
-                <Image 
-                    style={styles.avatar}
-                    source={{uri: "https://media.istockphoto.com/vectors/man-avatar-profile-male-face-icon-vector-illustration-vector-id1142192538"}}
-                />
-                <View style={{marginLeft: 20}}>
-                    <Text style={[styles.title, {marginTop: 15 , marginBottom: 5}]}>Vitor</Text>
-                    <Text style={styles.caption}>band</Text>
-                </View>
+                <View style={styles.perfilContainer}>
+                    <Image 
+                        style={styles.perfilAvatar}
+                        source={{uri: "https://media.istockphoto.com/vectors/man-avatar-profile-male-face-icon-vector-illustration-vector-id1142192538"}}
+                    />
 
+                    <View style={styles.perfilInfoContainer}>
+                        <Text style={[styles.perfilName]}>Vitor</Text>
+                        <Text style={styles.perfilCaption}>Band</Text>
+                    </View>
                 </View>
             </View>
 
             <View style={styles.userInfoSection}>
-                <View style={styles.row}>
-                    <Icon name="map-marker-radius" size={20} color="#777777"/>
-                    <Text style={{color: "#777777", marginLeft: 15}}>Rua pompeira, 58 - Osasco - SP</Text>
+                <View style={styles.infoRow}>
+                    <Icon name="map-marker-radius" size={20} style={styles.infoIconContainer}/>
+                    <Text style={styles.iconText}>Rua pompeira, 58 - Osasco - SP</Text>
                 </View>
-                <View style={styles.row}>
-                    <Icon name="phone" size={20} color="#777777"/>
-                    <Text style={{color: "#777777", marginLeft: 15}}>+55 11 3658-9245</Text>
+                <View style={styles.infoRow}>
+                    <Icon name="phone" size={20} style={styles.infoIconContainer}/>
+                    <Text style={styles.iconText}>+55 11 3658-9245</Text>
                 </View>
-                <View style={styles.row}>
-                    <Icon name="email" size={20} color="#777777"/>
-                    <Text style={{color: "#777777", marginLeft: 15}}>barDoze@email.com</Text>
+                <View style={styles.infoRow}>
+                    <Icon name="email" size={20} style={styles.infoIconContainer}/>
+                    <Text style={styles.iconText}>barDoze@email.com</Text>
                 </View>
             </View>
 
-            <View style={[styles.menuWrapper, {      
-                borderBottomColor: '#989C98',
-                borderTopColor: '#989C98',
-                borderTopWidth: 3,}]}>
+            <DivisorBar/>
+
+            <View style={styles.menuWrapper}>
                 <TouchableOpacity onPress={vaiParaEditProfile}>
                     <View style={styles.menuItem}>
-                        <Icon color="#FF6347" name="settings-outline" size={25}/>
+                        <Icon name="settings-outline" size={25} style={styles.optionIconContainer}/>
+
                         <Text style={styles.menuItemText}>Editar informações</Text>
                     </View>
                 </TouchableOpacity>
+
                 <TouchableOpacity>
                     <View style={styles.menuItem}>
-                        <Icon color="#FF6347" name="lock-outline" size={25}/>
+                        <Icon name="lock-outline" size={25} style={styles.optionIconContainer}/>
+
                         <Text style={styles.menuItemText}>Alterar senha</Text>
                     </View>
                 </TouchableOpacity>
+
                 <TouchableOpacity>
                     <View style={styles.menuItem}>
-                        <Icon color="#FF6347" name="heart-outline" size={25}/>
+                        <Icon name="heart-outline" size={25} style={styles.optionIconContainer}/>
+
                         <Text style={styles.menuItemText}>Favoritos</Text>
                     </View>
                 </TouchableOpacity>
+
                 <TouchableOpacity onPress={sair}>
                     <View style={styles.menuItem}>
-                        <Icon2 color="#FF6347" name="sign-out-alt" size={23}/>
+                        <Icon2 name="sign-out-alt" size={23} style={styles.optionIconContainer}/>
+
                         <Text style={styles.menuItemText}>Sair</Text>
                     </View>
                 </TouchableOpacity>
